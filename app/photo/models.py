b86130da732cdafca31a8f8390fd9cf23f1d5d1d
stygj <stygj59@gmail.com>
2024-01-13 11:56:02 +0900

    모델의 오타를 수정합니다.

--- app/photo/models.py
+++ app/photo/models.py
@@ -7,7 +7,7 @@ class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now())
     updated_at: Mapped[DateTime] = mapped_column(DateTime)
-    deleted: Mapped[DateTime] = mapped_column(DateTime)
+    deleted_at: Mapped[DateTime] = mapped_column(DateTime)
 
 
 class Photo(Base):
